4c839d33054f5a07bec9d1bc69152dae61a36457
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2023-08-26 10:23:37 -0500

    notes

--- primitive-types/activity.js
+++ primitive-types/activity.js
@@ -1,1 +1,4 @@
 console.log("helo from actiivt.js")
+
+//This is where you can play with the examples from https://digitalskills.instructure.com/courses/10576/pages/activity-code-review-js-1-primitive-types-and-objects?module_item_id=1426794
+// feel free to copy paste them or practice other examples to understand what I showed in my presentation
